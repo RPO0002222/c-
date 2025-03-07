8895648a78ebe4ef0f176673bf457773b9095987
RPO0002222 <ppavlo049@gmail.com>
2025-03-07 13:16:41 +0900

    Add files via upload

---
+++ класс круг.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+class Circle
+
+int main() {
+    Circle c1(5.0);
+    Circle c2(5.0);
+    Circle c3(10.0);
+
+    // ==
+    cout << "c1 и c2 " << (c1 == c2 ? "равны" : "не равны") << endl; 
+    // проверка  >
+    cout << "c3 > c1: " << (c3 > c1 ? "да" : "нет") << endl; 
+
+    // изменение радиуса
+    c1 += 3.0;
+    c2 -= 2.0;
+    cout << c1 << endl; 
+    cout << c2 << endl; 
+
+    return 0;
+}
